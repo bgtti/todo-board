18e7cb6d1eeac70753a515634e5873610a6de695
bgtti <bru.mgtti@gmail.com>
2022-08-09 15:15:00 +0200

    favicon fix and example todos added

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # todo-list
+
 Live preview: https://bgtti.github.io/todo-list/
 
 ![Preview of app](/src/assets/TodoAppPreview.png)
@@ -21,5 +22,13 @@ The code is organized in the following way:
 index.js: displays the elements in the html file. Most elements are imported from other files (navbar, footer, modals, etc).
 HTML elements are created in separate files than js management functions. Example: while boards-add-board-modal.js contains the HTML content of the modal, boards.js manages the injection function, and the index.js manages the event listeners.
 app.js: sets the arrays where todos and boards are saved to. Also used to manage local storage.
+
+How this Todo App could be improved:
+
+- Adding an option to order todos by priority or due date,
+- Adding a log-in and using a database to store data (security of input fields should be checked),
+- Adding the possibility to view todos as lists instead of cards,
+- Adding the possibility of changing priority of a todo by clicking on the priority icon,
+- UI design, especially text areas in modals.
 
 Odin Project Requirements: https://www.theodinproject.com/lessons/node-path-javascript-todo-list
--- src/module-00-app/app.js
+++ src/module-00-app/app.js
@@ -7,12 +7,31 @@ let allTodosArray; //the allTodosArray stores all active todos.
 let todoCounter; //the todoCounter is used to give boards a unique id
 
 //*************** LOCAL STORAGE FUNCTIONS ***************
+//either retrieves local storage data or uses example data:
 const retrieveLocalStorage = function () {
-    allBoardsArray = JSON.parse(localStorage.getItem("allBoards")) || [{ boardId: 'board0', boardName: 'All boards' }, { boardId: 'board1', boardName: 'Default' }];
+    allBoardsArray = JSON.parse(localStorage.getItem("allBoards")) || [{ boardId: 'board0', boardName: 'All boards' }, { boardId: 'board1', boardName: 'Default' }, { boardId: 'board2', boardName: 'The Odin Project' }];
     arrayCounter = JSON.parse(localStorage.getItem("boardsCounter")) || 2;
-    allTodosArray = JSON.parse(localStorage.getItem("allTodos")) || [{
-        toDoId: 'td0', title: 'To do example', boardId: 'board1', priority: '2', dueDate: '2022-05-28', description: 'With Todo Boards you can add new to do cards with a description, check-list, and notes. Click "Add to do" to start!', note: 'The description area is too short? Add notes to complement it!', checklist: [['Checklists', true], ['are', true], ['not', false], ['great!', true]]
-    }];
+    allTodosArray = JSON.parse(localStorage.getItem("allTodos")) || [
+        {
+            toDoId: 'td0', title: 'To do example', boardId: 'board1', priority: '2', dueDate: '2022-05-28', description: 'With Todo Boards you can add new to do cards with a description, check-list, and notes. Click "Add to do" to start! Ps: an icon will appear at the bottom of the card if you have added note and/or a checklist!', note: 'The description area is too short? Add notes to complement it!', checklist: [['Checklists', true], ['are', true], ['not', false], ['great!', true]]
+        },
+        {
+            toDoId: 'td1', title: 'Todo Project', boardId: 'board2', priority: '1', dueDate: '2022-05-30', description: 'Create a todo list with projects or separate lists of todos: use webpack and date-fns!', note: 'How about writing a note about your progress?', checklist: [['Create boards', true], ['Create Todos', true], ['Filter Schedule', true], ['Escape form fields', false]]
+        },
+        {
+            toDoId: 'td2', title: 'Library Project', boardId: 'board2', priority: '3', dueDate: '', description: 'Do a library app: store boo objects in simple array. Allow users to add, edir, and delete books & change book status to "read"', note: '', checklist: []
+        }
+        ,
+        {
+            toDoId: 'td3', title: 'Restaurant Page', boardId: 'board2', priority: '3', dueDate: '', description: 'Use webpack to create a restaurant page. Use tabbed browsing to access the Contact and Menu pages.', note: '', checklist: []
+        },
+        {
+            toDoId: 'td4', title: 'Weather App', boardId: 'board2', priority: '0', dueDate: '', description: 'Create a weather forecast site using the weather API', note: '', checklist: []
+        },
+        {
+            toDoId: 'td5', title: 'Battleship Game', boardId: 'board2', priority: '0', dueDate: '', description: 'Build the game Battleship to practice Test Driven Development with Jest.', note: '', checklist: []
+        }
+    ];
     todoCounter = JSON.parse(localStorage.getItem("todoCounter")) || 1;
 }()
 
